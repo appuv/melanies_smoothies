e1d2e938761085395be0f72fecdd511e2d622b8f
Appu <appukdr90@gmail.com>
2024-06-08 17:51:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,4 +38,4 @@ if time_to_insert:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
