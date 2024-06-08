472f6e195139ce1ecda0f514aff93343ff5af259
Appu <appukdr90@gmail.com>
2024-06-08 17:53:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,4 @@ if time_to_insert:
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_respon.json(),use_container_width=True)
+fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
